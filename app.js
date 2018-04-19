0714569ca077fd3764025d31126dbf3b2e2c4b18
Aderbal Farias <aderbalfarias@hotmail.com>
2018-04-19 02:34:04 -0300

    Updating references and template

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
-// Put this in the script section in JSFiddle
-// In a local setup, you need to merge this and the index.html file into one file
 new Vue({
-	el: '#app',
-	data: {
-		title: 'Hello World!'
-	}
-})
+    el: '#app',
+    data: {
+        title: 'Hello World!'
+    }
+});
+
+
+
