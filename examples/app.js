394bf046948de518d2a061e8b0a577bc5060ab60
Aderbal Farias <aderbalfarias@hotmail.com>
2018-04-26 00:12:45 -0300

    Including component

--- examples/app.js
+++ examples/app.js
@@ -5,4 +5,17 @@ new Vue({
         message: '',
         checkedNames: []
     }
+});
+
+Vue.component('button-counter', {
+    data: function () {
+        return {
+            count: 0
+        }
+    },
+    template: '<button v-on:click="count++">You clicked me {{ count }} times.</button>'
+})
+
+new Vue({ 
+    el: '#components-demo' 
 })
