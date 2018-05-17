aab1c34990f1416254e71e2d94635ecedbfd8311
Aderbal Farias <aderbalfarias@hotmail.com>
2018-05-17 02:56:54 -0300

    Changing tree

--- src/components/Tree.vue
+++ src/components/Tree.vue
@@ -1,6 +1,6 @@
 <template>
 <div>
-    <p>You can double click on an item to turn it into a folder.</p>
+     <p>You can double click on an item to turn it into a folder.</p>
 
     <script type="text/x-template" id="item-template">
         <li>
@@ -8,24 +8,24 @@
             :class="{bold: isFolder}"
             @click="toggle"
             @dblclick="changeType">
-            {{ model.title }}
+            {{ model.name }}
             <span v-if="isFolder">[{{ open ? '-' : '+' }}]</span>
             </div>
             <ul v-show="open" v-if="isFolder">
-                <Tree
+                <item
                 class="item"
                 v-for="(model, index) in model.children"
                 :key="index"
                 :model="model">
-                </Tree>
+                </item>
                 <li class="add" @click="addChild">+</li>
             </ul>
         </li>
     </script>
 
-    <ul>
-        <Tree class="item" :model="model">
-        </Tree>
+    <ul id="demo">
+        <item class="item" :model="treeData">
+        </item>
     </ul>
 </div>
 </template>
