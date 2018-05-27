c86c47075e0e571042c32d55d95be6f67cfc290d
Aderbal Farias <aderbalfarias@hotmail.com>
2018-05-27 02:15:24 -0300

    Including user component

---
+++ src/components/User/User.vue
@@ -0,0 +1,56 @@
+<template>
+    <div class="component">
+        <h1>The User Component</h1>
+        <p>I'm an awesome User!</p>
+        <button @click="changeName">Change my Name</button>
+        <p>Name is {{ name }}</p>
+        <p>Age is {{ age }}</p>
+        <hr>
+        <div class="row">
+            <div class="col-xs-12 col-sm-6">
+                <app-user-detail
+                    :myName="name"
+                    @nameWasReset="name = $event"
+                    :resetFn="resetName"
+                    :userAge="age">
+                </app-user-detail>
+            </div>
+            <div class="col-xs-12 col-sm-6">
+                <app-user-edit
+                    :userAge="age"
+                    @ageWasEdited="age = $event">
+                </app-user-edit>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import UserDetail from './UserDetail.vue';
+    import UserEdit from './UserEdit.vue';
+
+    export default {
+        name: 'User',
+        data: function () {
+            return {
+                name: 'Aderbal',
+                age: 25
+            };
+        },
+        methods: {
+            changeName() {
+                this.name = 'Anonymous';
+            },
+            resetName() {
+                this.name = 'Aderbal';
+            }
+        },
+        components: {
+            appUserDetail: UserDetail,
+            appUserEdit: UserEdit
+        }
+    }
+</script>
+
+<style scoped>
+</style>
---
+++ src/components/User/UserEdit.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="component">
+        <h3>User Edit</h3>
+        <p>User Age: {{ userAge }}</p>
+        <button @click="editAge">Edit Age</button>
+    </div>
+</template>
+
+<script>
+    import {eventBus} from '../main';
+
+    export default {
+        props: ['userAge'],
+        methods: {
+            editAge() {
+                this.userAge = 30;
+                // this.$emit('ageWasEdited', this.userAge);
+                // eventBus.$emit('ageWasEdited', this.userAge);
+                eventBus.changeAge(this.userAge);
+            }
+        }
+    }
+</script>
+
+<style scoped>
+</style>
