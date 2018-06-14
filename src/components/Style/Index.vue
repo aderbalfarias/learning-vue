0d38c779b80b1496836ca510e47929fc4670f5e1
Aderbal Farias <aderbalfarias@hotmail.com>
2018-06-13 23:58:55 -0300

    Including css animate

--- src/components/Style/Index.vue
+++ src/components/Style/Index.vue
@@ -1,12 +1,22 @@
 <template>
     <div class="container">
-        
+        <transition-group name="fadeLeft" tag="ul">
+            <li v-for="item in items" v-bind:key="item">
+                {{ item }}
+            </li>
+        </transition-group>
     </div>
 </template>
 
 <script>
     export default {
         name: 'Style',
+        data() {
+            return {
+                show: true,
+                items: [1, 2, 3, 4]
+            }
+        }
     }
 </script>
 
