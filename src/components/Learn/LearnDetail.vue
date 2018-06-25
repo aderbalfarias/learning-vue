318cdc07be799fb820997cfc949ba7632879e04a
Aderbal Farias <aderbalfarias@hotmail.com>
2018-06-24 22:22:22 -0300

    Code refactoring

--- src/components/Learn/LearnDetail.vue
+++ src/components/Learn/LearnDetail.vue
@@ -2,17 +2,31 @@
     <div>
         <h3>Learn Details</h3>
         <p>User loaded has ID: {{ $route.params.id }}</p>
-        <router-link
-            tag="button"
-            :to="{ name: 'learnEdit', params: { id: $route.params.id } }"
-            class="btn btn-primary">
-                Go to Edit 1
-        </router-link>
-        <router-link
-            tag="button"
-            :to="{ name: 'learnEdit', params: { id: $route.params.id }, query: { locale: 'en', q: 100 } }"
-            class="btn btn-primary">
-                Go to Edit 2
-        </router-link>
+        <router-link tag="button" :to="link1" class="btn btn-primary">Go to Edit 1</router-link>
+        <router-link tag="button" :to="link2" class="btn btn-primary">Go to Edit 2</router-link>
     </div>
 </template>
+<script>
+    export default {
+        data() {
+            return {
+                link1: {
+                    name: 'learnEdit', 
+                    params: { 
+                        id: this.$route.params.id 
+                    }
+                },
+                link2: {
+                    name: 'learnEdit', 
+                    params: { 
+                        id: this.$route.params.id 
+                    },
+                    query: { 
+                        locale: 'en', 
+                        q: 100 
+                    }
+                }
+            }
+        }
+    }
+</script>
