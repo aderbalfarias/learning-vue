8558b34f177afa232533309df3cec13f10764855
Aderbal Farias <aderbalfarias@hotmail.com>
2018-06-25 00:31:22 -0300

    Adding transition to router-view

--- src/components/Learn/Learn.vue
+++ src/components/Learn/Learn.vue
@@ -4,7 +4,9 @@
         <hr>
         <button @click="navigateToHome" class="btn btn-primary">Go to Home</button>
         <hr>
-        <router-view></router-view>
+        <transition name="slide" mode="out-in">
+            <router-view></router-view>
+        </transition>
     </div>
 </template>
 
@@ -18,3 +20,38 @@
         }
     }
 </script>
+
+<style>
+    .slide-leave-active {
+        transition: opacity 1s ease;
+        opacity: 0;
+        animation: slide-out 1s ease-out forwards;
+    }
+
+    .slide-leave {
+        opacity: 1;
+        transform: translateX(0);
+    }
+
+    .slide-enter-active {
+        animation: slide-in 1s ease-out forwards;
+    }
+
+    @keyframes slide-out {
+        0% {
+            transform: translateY(0);
+        }
+        100% {
+            transform: translateY(-30px);
+        }
+    }
+
+    @keyframes slide-in {
+        0% {
+            transform: translateY(-30px);
+        }
+        100% {
+            transform: translateY(0);
+        }
+    }
+</style>
