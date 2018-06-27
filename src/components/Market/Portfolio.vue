2d9f64477335a5c904538e949d175d8d9e48a338
Aderbal Farias <aderbalfarias@hotmail.com>
2018-06-27 01:03:11 -0300

    Including a new component

---
+++ src/components/Market/Portfolio.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <app-sell-stock v-for="(stock, index) in stocks" :stock="stock" :key="index"></app-sell-stock>
+    </div>
+</template>
+
+<script>
+    import {mapGetters} from 'vuex';
+    import SellStock from './SellStock.vue';
+
+    export default {
+        computed: {
+            ...mapGetters({
+                stocks: 'stockPortfolio'
+            })
+        },
+        components: {
+            appSellStock: SellStock
+        }
+    }
+</script>
---
+++ src/components/Market/Stocks.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <app-buy-stock v-for="(stock, index) in stocks" :stock="stock" :key="index"></app-buy-stock>
+    </div>
+</template>
+
+<script>
+    import BuyStock from './BuyStock.vue';
+
+    export default {
+        components: {
+            appBuyStock: BuyStock
+        },
+        computed: {
+            stocks() {
+                return this.$store.getters.stocks;
+            }
+        }
+    }
+</script>
